9d6d28fa5c79e8b42dce0ff53fcaab02589c82be
jahnavi-gottam <148139086+jahnavi-gottam@users.noreply.github.com>
2026-09-19 22:17:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code, index) => {
+  code.addEventListener("input", (e) => {
+    code.value = code.value.replace(/\D/g, "");
+
+    if (code.value && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
+
+  code.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (code.value === "" && index > 0) {
+        codes[index - 1].focus();
+        codes[index - 1].value = "";
+      }
+    }
+  });
+});
